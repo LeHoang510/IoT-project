eb29ff20d54a6d252b0fc3b428129ddda24b7ed5
le-hoang.nguyen <lehoangnguyen510@gmail.com>
2023-11-13 14:52:21 +0100

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,55 @@
 #include <M5Core2.h>
 #include "EnvSensor.hpp"
 #include "M5_ENV.h"
-#include <WiFi.h>
 #include <PubSubClient.h>
 #include <ArduinoJson.h>
 #include <vector>
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include <string.h>
+#define NBSENSOR_MAX 16
+
+
 
 const char * ssid = "WIFI_TP_IOT";
 const char * password = "TP_IOT_2022";
+String mac;
 
-#define NBSENSOR_MAX 16
+WiFiClient wifiClient;
+PubSubClient client(wifiClient);
+
 std::vector<EnvSensor> sensors;
 TFT_eSprite tftSprite = TFT_eSprite(&M5.Lcd);
 uint32_t now;
 IPAddress server(192, 168, 31, 134);
 
+SHT3X sht30;
+QMP6988 qmp6988;
 
+float tmp      = 0.0;
+float hum      = 0.0;
+float pressure = 0.0;
+
+
+const char* host = "api.thingspeak.com"; // This should not be changed
+const int httpPort = 80; // This should not be changed
+
+// The default example accepts one data filed named "field1"
+// For your own server you can ofcourse create more of them.
+int field1 = 0;
+
+int numberOfResults = 3; // Number of results to be read
+int fieldNumber = 1; // Field number which will be read out
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i=0;i<length;i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+}
 
 void printLCD(){
   
@@ -53,20 +87,97 @@ void printLCD(){
 
 
 void setup() {
-    M5.begin();
-    now = millis();
-    tftSprite.setColorDepth(8);
-    tftSprite.createSprite(320, 240);
-    Serial.println("Fin du Setup");
+
+    M5.begin();             // Init M5Core2.  初始化M5Core2
+    M5.lcd.setTextSize(2);  // Set the text size to 2.  设置文字大小为2
+    Wire.begin();  // Wire init, adding the I2C bus.  Wire初始化, 加入i2c总线
+    sht30.init();
+    qmp6988.init();
+    M5.lcd.println(F("ENVIII Unit(SHT30 and QMP6988) test"));
+
+    Serial.begin(115200);
+    while(!Serial){delay(100);}
+
+    // We start by connecting to a WiFi network
+
+    Serial.println();
+    Serial.println("******************************************************");
+    Serial.print("Connecting to ");
+    Serial.println(ssid);
+
+    WiFi.begin(ssid, password);
+
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+    Serial.println("MAC Address: ");
+    Serial.println(WiFi.macAddress());
+    mac = WiFi.macAddress();
+    delay(10000);
+
+    client.setServer(server, 1883);
+    client.setCallback(callback);
+
 }
 
-
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect("mac")) {
+      Serial.println("connected");
+      client.publish("outTopic","hello world");
+      // ... and resubscribe
+      client.subscribe("inTopic");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
 
 void loop() {
-  float tmp, hum, pressure;
-    if(millis() - now > 1000){
-        Serial.println("Loop");
-        now = millis();
-        printLCD();
-    }
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
+  delay(10000);
+
+  pressure = qmp6988.calcPressure();
+  if (sht30.get() == 0) {  // Obtain the data of shT30.  获取sht30的数据
+      tmp = sht30.cTemp;   // Store the temperature obtained from shT30.
+      hum = sht30.humidity;  // Store the humidity obtained from the SHT30.
+  } else {
+      tmp = 0, hum = 0;
+  }
+  M5.lcd.fillRect(0, 20, 100, 60, BLACK);  // Fill the screen with black 
+  M5.lcd.setCursor(0, 20);
+  M5.Lcd.printf("Temp: %2.1f  \r\nHumi: %2.0f%%  \r\nPressure:%2.0fPa\r\n",
+                tmp, hum, pressure);
+
+  DynamicJsonDocument doc(1024);
+  String output;
+  doc["humidity"] = hum;
+  doc["temperature"]   = tmp;
+  doc["pressure"]= pressure;
+  doc["mac"] = mac;
+
+  serializeJson(doc, output);
+
+  Serial.println(output);
+  client.publish("Env/3C:61:05:0D:CA:38", output.c_str());
+  client.subscribe("Env/#");
+
+  delay(2000);
 }
